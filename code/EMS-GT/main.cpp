c74fd3123dcd146ab8a1ee1c59847e383410a9ce
Mark Ronquillo <markronquillo23@gmail.com>
2016-06-29 14:31:07 +0800

    Evaluation updated n' values

--- code/EMS-GT/main.cpp
+++ code/EMS-GT/main.cpp
@@ -9,35 +9,40 @@ using namespace std;
 
 // TODO:
 // make a generic EMS_GT, using long and int (32 vs 64)
-// 
+//
 int main(int argc, char* argv[]) {
-	// string inputFileHeader = "../datasets/";
-	string inputFileName;
-	int tPrime = -1;
+    // string inputFileHeader = "../datasets/";
+    string inputFileName;
+    int tPrime = -1;
 
     DataSetParams dsParams;
 
-    if (argc > 1)   
+    if (argc > 1)
         inputFileName = argv[1];
     else
         inputFileName = "17,6,0"; // hardcoded muna
 
-    if (argc > 2)
-    	tPrime = atoi(argv[2]);
-
     // put the dataset config and put it in the dsConfig struct
-	readInput(inputFileName, &dsParams);
+    readInput(inputFileName, &dsParams);
 
-	// other config details are based on the dataset parameters
-	// so we need to pass it to
+    // other config details are based on the dataset parameters
+    // so we need to pass it to
     Config config = Config(&dsParams);
 
-    // change if argument is passed
-    if (tPrime != -1)
-    	config.tPrime = tPrime;
+    // adjust tPrime (nPrime) value depending on the dataset
+    // hardcoded
+    switch(dsParams.lengthOfMotif)  {
+        case 13: config.tPrime = 9; break;
+        case 15: config.tPrime = 8; break;
+        case 17: config.tPrime = 7; break;
+    }
 
-	EMS_GT ems = EMS_GT(&dsParams, &config);
-	ems.start();
+    // override change if argument is passed
+    if (argc > 2)
+        config.tPrime = atoi(argv[2]);
 
-	return 0;
+    EMS_GT ems = EMS_GT(&dsParams, &config);
+    ems.start();
+
+    return 0;
 }
